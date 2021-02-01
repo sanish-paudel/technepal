7155937134a19a03b41d8a10c8e500fc27f2194e
sanish-paudel <65480128+sanish-paudel@users.noreply.github.com>
2021-02-01 09:30:48 +0545

    Add files via upload

---
+++ assets/js/main.js
@@ -0,0 +1,10 @@
+function myfunction(){
+	let element = document.getElementsByClassName('hero-heading');
+	alert("website is under built")
+}
+
+function textfunction(){
+	let element = document.getElementsByClassName('hero-heading');
+	element[0].style.color ='purple';
+	console.log(element[0])
+}
